571f8c1faec6542dfa5b73ba1be982b040f4e7f5
HarshShrimal14 <harshshrimal014@gmail.com>
2024-04-10 16:20:06 +0530

    hide details

--- server/index.js
+++ server/index.js
@@ -7,6 +7,9 @@ import postRoutes from "./routes/posts.js"
 
 const app = express();
 
+require('dotenv').config();
+const username= process.env.USERNAME;
+const password= process.env.PASSWORD;
 
 
 app.use(bodyParser.json({ limit : "30mb" , extended : "true"}));
@@ -15,7 +18,7 @@ app.use(cors());
 
 app.use('/posts' , postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://harshshrimal:harshshrimal123@cluster0.vgd8gx0.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = "mongodb+srv://" +username+ ":" +password + "@cluster0.vgd8gx0.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 // mongoose.connect(CONNECTION_URL)
